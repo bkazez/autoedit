a12b57bb44e54e44c0ea9d923365936670dc6b7e
Ben Kazez <64005+bkazez@users.noreply.github.com>
2024-06-21 22:35:30 -0700

    Improve formatting and outfile arg

--- optimal_edl.py
+++ optimal_edl.py
@@ -64,12 +64,12 @@ Source Table Entries: {num_sources}
     source_ids = {}
     for i, filepath in enumerate(filepaths):
         source_ids[filepath] = i + 1
-        wav_filename = filepath.replace('.csv', '.wav')
-        source_entries += f"   {i+1} \"{wav_filename}\"\n"
+        wav_filepath = filepath.replace('.csv', '.wav')
+        source_entries += f"   {i+1} \"{wav_filepath}\"\n"
 
     track_header = """Track 1: "Main" Solo: 0 Mute: 0
 #Source      Track       Play-In     Play-Out    Record-In   Record-Out  Vol(dB)  MT LK FadeIn  %     CurveType    FadeOut  %     CurveType    Name
-#----------- ----------- ----------- ----------- ----------- ----------- -------- -- -- ------- ----- ----------- -------- ----- ------------ -----------
+#----------- ----------- ----------- ----------- ----------- ----------- -------- -- -- ------- ----- ------------ -------- ----- ------------ -----------
 """
 
     track_entries = ""
@@ -93,7 +93,7 @@ Source Table Entries: {num_sources}
 
         wav_filename = os.path.basename(filepaths[take]).replace('.csv', '.wav')
         source_id = source_ids[filepaths[take]]
-        track_entries += "{:>11d} {:>11d} {:>11d} {:>11d} {:>11d} {:>11d} {:>8} {:>2} {:>2} {:>7} {:>5}  {:<12} {:>6} {:>5}  {:<11}  {}\n".format(
+        track_entries += "{:>11d} {:>11d} {:>11d} {:>11d} {:>11d} {:>11d} {:>8} {:>2} {:>2} {:>7} {:>5} {:>12} {:>8} {:>5} {:>12} {}\n".format(
             source_id, 1, play_in, play_out, record_in, record_out, "0.00", 0, 0, 0, 0, "\"*default\"", 0, 0, "\"*default\"", f"\"{wav_filename}\""
         )
 
@@ -103,7 +103,7 @@ Source Table Entries: {num_sources}
     edl_content = edl_header.format(num_sources=len(filepaths)) + source_entries + "\n" + track_header + track_entries
     return edl_content
 
-def main(filepaths, edit_penalty):
+def main(filepaths, outfile, edit_penalty):
     dfs = read_csv_files(filepaths)
     optimal_path, best_score = calculate_optimal_path(dfs, edit_penalty)
 
@@ -116,13 +116,14 @@ def main(filepaths, edit_penalty):
         print(f'Measure: {measure}, Take: {take_names[take]}')
 
     edl_content = generate_samplitude_edl(optimal_path, filepaths, dfs)
-    with open("output.edl", "w") as edl_file:
+    with open(outfile, "w") as edl_file:
         edl_file.write(edl_content)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Find the optimal edit decision list from CSV files.')
     parser.add_argument('files', metavar='F', type=str, nargs='+', help='CSV files containing the ratings')
+    parser.add_argument('--outfile', type=str, help='Output EDL file')
     parser.add_argument('--edit_penalty', type=float, default=0.1, help='Penalty for changing takes')
 
     args = parser.parse_args()
-    main(args.files, args.edit_penalty)
+    main(args.files, args.outfile, args.edit_penalty)
